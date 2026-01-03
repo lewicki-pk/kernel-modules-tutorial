497ea15e5dffbd2e1404cd0ae2abbbcdc72f8269
Piotr Lewicki <lewicki.pk@gmail.com>
2026-01-03 15:12:20 +0100

    Improvements to basic hello

--- 02-better-hello/hello.c
+++ 02-better-hello/hello.c
@@ -1,12 +1,12 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-int my_init(void) {
+static int __init my_init(void) {
     printk("Lewiatan - hello from module\n");
     return 0;
 }
 
-void my_exit(void) {
+static void __exit my_exit(void) {
     printk("Lewiatan - goodbye from module\n");
 }
 
@@ -14,3 +14,5 @@ module_init(my_init);
 module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Piotr Lewicki");
+MODULE_DESCRIPTION("Basic kernel module that has init and exit functions - improved version");
